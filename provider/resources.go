ce1162f75d3ac4208f45de69613997c91f0f1fa6
Venelin <venelin@pulumi.com>
2024-03-20 16:33:09 +0200

    ci-mgmt

--- provider/resources.go
+++ provider/resources.go
@@ -21,14 +21,14 @@ import (
 	// Allow embedding bridge-metadata.json in the provider.
 	_ "embed"
 
+	// Replace this provider with the provider you are bridging.
+	xyz "github.com/iwahbe/terraform-provider-xyz/provider"
+
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/tokens"
 	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
-
-	// Replace this provider with the provider you are bridging.
-	xyz "github.com/iwahbe/terraform-provider-xyz/provider"
 
 	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
 )
