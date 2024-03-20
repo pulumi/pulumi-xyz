b70a6bc1f60ef6c1adf7dba0c91bb58d2dbd11be
Venelin <venelin@pulumi.com>
2024-03-20 17:15:31 +0200

    add version hack

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -1,7 +1,12 @@
 package provider
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
+)
 
 func TestProvider(_ *testing.T) {
+	version.Version = "1.0.0"
 	Provider()
 }
