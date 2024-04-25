4c29cdb841ee3d377df710aa8f3094cf465a338b
Daniel Bradley <daniel.dbphoto@gmail.com>
2024-04-25 13:32:41 +0100

    Remove renamed datasource (#74)
    This causes issues when we fix the major version. Pulumictl currently returns version 0.0.1... but the provider is meant to be on version 1, so when actually using version 1.x it then removes this obsolete datasource causing a diff.

--- sdk/python/pulumi_xyz/__init__.py
+++ sdk/python/pulumi_xyz/__init__.py
@@ -6,7 +6,6 @@ from . import _utilities
 import typing
 # Export this package's modules as members:
 from .data_source import *
-from .get_data_source import *
 from .provider import *
 from .resource import *
 _utilities.register(
